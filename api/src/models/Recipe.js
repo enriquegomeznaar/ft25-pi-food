18ec300b1c50e85731c18065709421fd5dcb8aae
enriquegomeznaar <enriquegomeznaar@gmail.com>
2022-06-05 02:44:22 -0300

    commit

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,36 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
+  return sequelize.define('recipe', { 
+    title: {
+      type: DataTypes.STRING, 
       allowNull: false,
     },
+    id: { 
+      type: DataTypes.UUID,
+      primaryKey: true, 
+      defaultValue: DataTypes.UUIDV4
+    },  
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    spoonacularScore: {
+      type: DataTypes.INTEGER
+    },
+    healthScore: {
+      type: DataTypes.INTEGER
+    },
+    analyzedInstructions: {
+      type: DataTypes.ARRAY(DataTypes.TEXT)
+    }, 
+    image : {
+      type: DataTypes.STRING
+    } 
   });
+  // createInDB(video de selena)
+
 };
